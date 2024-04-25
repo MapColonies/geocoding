7b49621a59bb02e996e1c0c3c4875e90e4f815f3
Niv Greenstein <greenstein9@gmail.com>
2024-04-25 12:58:38 +0300

    feat: created route repository

---
+++ src/route/DAL/routeRepository.ts
@@ -0,0 +1,40 @@
+import { Client, estypes } from '@elastic/elasticsearch';
+import { FactoryFunction } from 'tsyringe';
+import { elasticClientSymbol } from '../../common/elastic';
+import { Route } from '../models/route';
+import { additionalSearchProperties } from '../../common/utils';
+import { RouteQueryParams, queryForControlPointInRoute, queryForRoute } from './queries';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createRouteRepository = (client: Client) => {
+  return {
+    async getRoutes(routeQueryParams: RouteQueryParams, size: number): Promise<estypes.SearchResponse<Route>> {
+      const response = await client.search<Route>({
+        ...additionalSearchProperties(size),
+        body: queryForRoute(routeQueryParams),
+      });
+
+      return response;
+    },
+
+    async getControlPointInRoute(
+      routeQueryParams: RouteQueryParams & Required<Pick<RouteQueryParams, 'controlPoint'>>,
+      size: number
+    ): Promise<estypes.SearchResponse<Route>> {
+      const response = await client.search<Route>({
+        ...additionalSearchProperties(size),
+        body: queryForControlPointInRoute(routeQueryParams),
+      });
+
+      return response;
+    },
+  };
+};
+
+export type RouteRepository = ReturnType<typeof createRouteRepository>;
+
+export const routeRepositoryFactory: FactoryFunction<RouteRepository> = (depContainer) => {
+  return createRouteRepository(depContainer.resolve<Client>(elasticClientSymbol));
+};
+
+export const ROUTE_REPOSITORY_SYMBOL = Symbol('ROUTE_REPOSITORY_SYMBOL');
