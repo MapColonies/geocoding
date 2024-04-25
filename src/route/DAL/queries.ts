d6987b491a5ac56e18c36587d2febd5f6643772c
Niv Greenstein <greenstein9@gmail.com>
2024-04-25 12:58:25 +0300

    feat: created elastic query to query item

---
+++ src/route/DAL/queries.ts
@@ -0,0 +1,90 @@
+import { estypes } from '@elastic/elasticsearch';
+import { boundingBox, geoDistance } from '../../common/elastic/utils';
+import { Route } from '../models/route';
+import { GeoContext } from '../../common/interfaces';
+
+export interface RouteQueryParams {
+  commandName: string;
+  controlPoint?: number;
+  geo?: GeoContext;
+}
+
+/* eslint-disable @typescript-eslint/naming-convention */
+export const queryForRoute = (params: RouteQueryParams): estypes.SearchRequest => ({
+  query: {
+    bool: {
+      should: [
+        {
+          term: {
+            'properties.TYPE.keyword': 'ROUTE',
+          },
+        },
+      ],
+      must: [
+        {
+          match: {
+            'properties.OBJECT_COMMAND_NAME.keyword': {
+              query: params.commandName,
+              fuzziness: 1,
+              prefix_length: 1,
+            },
+          },
+        },
+        ...(params.geo?.bbox ? [boundingBox(params.geo.bbox)] : []),
+        ...(params.geo?.radius
+          ? [
+              geoDistance(
+                params.geo as {
+                  radius: number;
+                  lon: number;
+                  lat: number;
+                }
+              ),
+            ]
+          : []),
+      ],
+    },
+  },
+});
+
+export const queryForControlPointInRoute = (params: RouteQueryParams & Required<Pick<RouteQueryParams, 'controlPoint'>>): estypes.SearchRequest => ({
+  query: {
+    bool: {
+      must: [
+        {
+          match: {
+            'properties.OBJECT_COMMAND_NAME.keyword': {
+              query: params.controlPoint,
+              fuzziness: 1,
+              prefix_length: 1,
+            },
+          },
+        },
+        ...(params.geo?.bbox ? [boundingBox(params.geo.bbox)] : []),
+        ...(params.geo?.radius
+          ? [
+              geoDistance(
+                params.geo as {
+                  radius: number;
+                  lon: number;
+                  lat: number;
+                }
+              ),
+            ]
+          : []),
+      ],
+      filter: [
+        {
+          terms: {
+            'properties.LAYER_NAME.keyword': ['CONTROL_GIL_GDB.CTR_CONTROL_POINT_N', 'CONTROL_GIL_GDB.CTR_CONTROL_POINT_CROSS_N'],
+          },
+        },
+        {
+          term: {
+            'properties.TIED_TO': params.commandName,
+          },
+        },
+      ],
+    },
+  },
+});
