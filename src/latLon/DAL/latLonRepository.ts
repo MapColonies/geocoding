5ea8432b8958c23995733ae77b1d68ab05c07d58
Niv Greenstein <niv@rafael.co.il>
2024-04-28 12:35:39 +0300

    feat: created custom repository for latLon

---
+++ src/latLon/DAL/latLonRepository.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { DataSource } from 'typeorm';
+import { FactoryFunction } from 'tsyringe';
+import { LatLon as LatLonDb } from './latLon';
+
+const createLatLonRepository = (dataSource: DataSource) => {
+  return dataSource.getRepository(LatLonDb).extend({
+    async latLonToTile({ x, y, zone }: { x: number; y: number; zone: number }): Promise<LatLonDb | null> {
+      const result = await this.findOne({
+        where: {
+          minX: x.toString(),
+          minY: y.toString(),
+          zone: zone.toString(),
+        },
+      });
+
+      return result;
+    },
+
+    async tileToLatLon(tileName: string): Promise<LatLonDb | null> {
+      const result = await this.findOne({
+        where: {
+          tileName,
+        },
+      });
+
+      return result;
+    },
+  });
+};
+
+export type LatLonRepository = ReturnType<typeof createLatLonRepository>;
+
+export const latLonRepositoryFactory: FactoryFunction<LatLonRepository> = (depContainer) => {
+  return createLatLonRepository(depContainer.resolve<DataSource>(DataSource));
+};
+
+export const LATLON_CUSTOM_REPOSITORY_SYMBOL = Symbol('LATLON_CUSTOM_REPOSITORY_SYMBOL');
