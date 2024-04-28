beee02ec0d37bcd490b87e217e8f4fa89e099a0a
Niv Greenstein <greenstein9@gmail.com>
2024-04-28 08:25:04 +0300

    feat: created latlon interface

---
+++ src/latLon/models/latLon.ts
@@ -0,0 +1,11 @@
+export interface LatLon {
+  primaryKey: number;
+  tileName: string;
+  zone: string;
+  minX: string;
+  minY: string;
+  extMinX: number;
+  extMinY: number;
+  extMaxX: number;
+  extMaxY: number;
+}
