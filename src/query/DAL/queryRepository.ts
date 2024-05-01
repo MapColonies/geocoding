c3324f4bbfee9d0d949675eeb012ec37783b07a5
Niv Greenstein <niv@rafael.co.il>
2024-05-01 16:25:08 +0300

    feat: created query repository

---
+++ src/query/DAL/queryRepository.ts
@@ -0,0 +1,62 @@
+import config from 'config';
+import { Client, estypes } from '@elastic/elasticsearch';
+import { FactoryFunction } from 'tsyringe';
+import { elasticClientSymbol } from '../../common/elastic';
+import { cleanQuery, fetchNLPService } from '../utils';
+import { PlaceType, TextSearchParams, TokenResponse } from '../interfaces';
+import { TextSearchHit } from '../models/textSearchHit';
+import { esQuery } from './queries';
+
+/* eslint-enable @typescript-eslint/naming-convention */
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createQueryRepository = (client: Client) => {
+  return {
+    async extractName(endpoint: string, query: string): Promise<string> {
+      const tokensRaw = cleanQuery(query);
+      const response = await fetchNLPService<TokenResponse>(endpoint, { tokens: tokensRaw });
+
+      const { tokens, prediction } = response[0];
+
+      if (!tokens || !prediction) {
+        throw new Error('No tokens or prediction');
+      }
+
+      const nameTokens = tokens.filter((_, index) => prediction[index] === 'name');
+
+      return nameTokens.join(' ');
+    },
+
+    async getPlaceType(endpoint: string, query: string): Promise<PlaceType> {
+      // const endpoint = config.get<string>('services.placeTypeUrl');
+      const response = await fetchNLPService<PlaceType>(endpoint, {
+        text: query,
+        start: 0,
+        end: query.length,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        entity_text: query,
+      });
+
+      return response[0];
+    },
+
+    async queryElastic(params: TextSearchParams): Promise<estypes.SearchResponse<TextSearchHit>> {
+      const index = config.get<string>('db.elastic.properties.nlpIndex');
+
+      const response = await client.search<TextSearchHit>({
+        index,
+        body: esQuery(params),
+      });
+
+      return response;
+    },
+  };
+};
+
+export type QueryRepository = ReturnType<typeof createQueryRepository>;
+
+export const itemRepositoryFactory: FactoryFunction<QueryRepository> = (depContainer) => {
+  return createQueryRepository(depContainer.resolve<Client>(elasticClientSymbol));
+};
+
+export const QUERY_REPOSITORY_SYMBOL = Symbol('QUERY_REPOSITORY_SYMBOL');
