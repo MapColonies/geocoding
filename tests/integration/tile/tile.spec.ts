51b2785472a7d545b6e92fd3a794a5fc6f52df3e
Niv Greenstein <greenstein9@gmail.com>
2024-05-06 13:13:40 +0300

    feat: created tiles integration tests

---
+++ tests/integration/tile/tile.spec.ts
@@ -0,0 +1,57 @@
+import jsLogger from '@map-colonies/js-logger';
+import { trace } from '@opentelemetry/api';
+import httpStatusCodes from 'http-status-codes';
+import { getApp } from '../../../src/app';
+import { SERVICES } from '../../../src/common/constants';
+import { GetTilesQueryParams } from '../../../src/tile/controllers/tileController';
+import { TileRequestSender } from './helpers/requestSender';
+
+describe('resourceName', function () {
+  let requestSender: TileRequestSender;
+
+  beforeEach(async function () {
+    // elasticClient = await initElasticsearchClient(elasticDataSourceOptions);
+    const app = await getApp({
+      override: [
+        { token: SERVICES.LOGGER, provider: { useValue: jsLogger({ enabled: false }) } },
+        { token: SERVICES.TRACER, provider: { useValue: trace.getTracer('testTracer') } },
+      ],
+      useChild: true,
+    });
+    requestSender = new TileRequestSender(app.app);
+  });
+
+  describe('Happy Path', function () {
+    it('should return 200 status code and the resource', async function () {
+      const response = await requestSender.getTiles({ tile: 'בעל' });
+
+      expect(response.status).toBe(httpStatusCodes.OK);
+      expect(response).toSatisfyApiSpec();
+    });
+  });
+  describe('Bad Path', function () {
+    // All requests with status code of 400
+    it('Should return 400 status code and meessage "request/query must have required property \'tile\'"', async function () {
+      const message = "request/query must have required property 'tile'";
+
+      const response = await requestSender.getTiles();
+
+      expect(response.status).toBe(httpStatusCodes.BAD_REQUEST);
+      expect(response.body).toMatchObject({ message });
+      expect(response).toSatisfyApiSpec();
+    });
+    it('Should return 400 status code for unknown parameter', async function () {
+      const parameter = 'test1234';
+      const message = `Unknown query parameter '${parameter}'`;
+
+      const response = await requestSender.getTiles({ [parameter]: parameter } as unknown as GetTilesQueryParams);
+
+      expect(response.status).toBe(httpStatusCodes.BAD_REQUEST);
+      expect(response.body).toMatchObject({ message });
+      expect(response).toSatisfyApiSpec();
+    });
+  });
+  describe('Sad Path', function () {
+    // All requests with status code 4XX-5XX
+  });
+});
