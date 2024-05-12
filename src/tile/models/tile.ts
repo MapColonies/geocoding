4b4d853a22375f8183fdb19c1e15dbdedd8cf223
Niv Greenstein <niv@rafael.co.il>
2024-05-12 16:13:57 +0300

    fix(tile): removed properties. now we accept every key as a property

--- src/tile/models/tile.ts
+++ src/tile/models/tile.ts
@@ -1,14 +1,3 @@
-import { Geometry, Feature } from 'geojson';
+import { Feature } from 'geojson';
 
-/* eslint-disable @typescript-eslint/naming-convention */
-interface Properties {
-  OBJECTID: number;
-  ZONE: string;
-  TILE_NAME: string;
-  SUB_TILE_ID?: string;
-  TILE_ID: null | string;
-  LAYER_NAME: string;
-  TYPE: string;
-}
-
-export interface Tile extends Feature<Geometry, Properties> {}
+export interface Tile extends Feature {}
