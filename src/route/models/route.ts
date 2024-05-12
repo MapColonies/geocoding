4adaf7826edecb5d51ec3d2147bad3abe93d96f4
Niv Greenstein <niv@rafael.co.il>
2024-05-12 16:13:47 +0300

    fix(route): removed properties. now we accept every key as a property

--- src/route/models/route.ts
+++ src/route/models/route.ts
@@ -1,16 +1,3 @@
-import { Feature, Geometry } from 'geojson';
+import { Feature } from 'geojson';
 
-/* eslint-disable @typescript-eslint/naming-convention */
-interface Properties {
-  OBJECTID: number;
-  OBJECT_COMMAND_NAME: string;
-  FIRST_GFID: null; // TODO: findout real type
-  SHAPE_Length: number;
-  F_CODE: number;
-  F_ATT: number;
-  LAYER_NAME: string;
-  ENTITY_HEB: string;
-  TYPE: string;
-}
-
-export interface Route extends Feature<Geometry, Properties> {}
+export interface Route extends Feature {}
