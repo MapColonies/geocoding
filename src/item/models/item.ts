63fc5f9fe7c9fb9e89cab14d59918caea3924fc8
Niv Greenstein <niv@rafael.co.il>
2024-05-12 16:13:36 +0300

    fix(item): removed properties. now we accept every key as a property

--- src/item/models/item.ts
+++ src/item/models/item.ts
@@ -1,23 +1,3 @@
-import { Feature, Geometry } from 'geojson';
+import { Feature } from 'geojson';
 
-/* eslint-disable @typescript-eslint/naming-convention */
-interface Properties {
-  OBJECTID: number;
-  F_CODE: number;
-  F_ATT: number;
-  VIEW_SCALE_50K_CONTROL: number;
-  NAME: null; // TODO: Check if this is correct
-  GFID: string;
-  OBJECT_COMMAND_NAME: string;
-  SUB_TILE_NAME: null; // TODO: Check if this is correct
-  TILE_NAME: string;
-  TILE_ID: null | string;
-  SUB_TILE_ID?: string;
-  'SHAPE.AREA': number;
-  'SHAPE.LEN': number;
-  LAYER_NAME: string;
-  ENTITY_HEB: string;
-  TYPE: string;
-}
-
-export interface Item extends Feature<Geometry, Properties> {}
+export interface Item extends Feature {}
