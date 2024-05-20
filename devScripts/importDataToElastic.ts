f1c37e4c8b7dfb21b1672f9d32ee2ed8f86a9e11
Niv Greenstein <niv@rafael.co.il>
2024-05-20 18:23:14 +0300

    feat: added nlp data import to it's elastic

--- devScripts/importDataToElastic.ts
+++ devScripts/importDataToElastic.ts
@@ -4,13 +4,38 @@
 import { Client } from '@elastic/elasticsearch';
 import config from '../config/test.json';
 import data from './elasticsearchData.json';
+import nlpData from './elasticNlpData.json';
 
 const main = async (): Promise<void> => {
-  const client = new Client({ node: config.db.elastic.searchy.node as string });
+  let client = new Client({ node: config.db.elastic.searchy.node });
 
   for (const item of data) {
     await client.index({
-      index: config.db.elastic.searchy.properties.index as string,
+      index: config.db.elastic.searchy.properties.index,
+      id: item._id,
+      body: item._source,
+    });
+  }
+
+  client = new Client({ node: config.db.elastic.nlp.node });
+
+  const indexExists = await client.indices.exists({ index: config.db.elastic.nlp.properties.index });
+  if (!indexExists) {
+    await client.indices.create({
+      index: config.db.elastic.nlp.properties.index,
+      body: {
+        mappings: {
+          properties: {
+            geo_json: { type: 'geo_shape' },
+          },
+        },
+      },
+    });
+  }
+
+  for (const item of nlpData) {
+    await client.index({
+      index: config.db.elastic.nlp.properties.index,
       id: item._id,
       body: item._source,
     });
