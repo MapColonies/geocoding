6af50f036eb0a2a260291d95498257e8dde4ba22
Niv Greenstein <niv@rafael.co.il>
2024-07-23 21:24:40 +0300

    fix: fixed types

--- src/item/models/itemManager.ts
+++ src/item/models/itemManager.ts
@@ -2,11 +2,11 @@ import { IConfig } from 'config';
 import { Logger } from '@map-colonies/js-logger';
 import { inject, injectable } from 'tsyringe';
 import { estypes } from '@elastic/elasticsearch';
-import { SERVICES } from '../../common/constants';
+import { SERVICES, elasticConfigPath } from '../../common/constants';
 import { ITEM_REPOSITORY_SYMBOL, ItemRepository } from '../DAL/itemRepository';
 import { ItemQueryParams } from '../DAL/queries';
 import { formatResponse } from '../../common/utils';
-import { FeatureCollection } from '../../common/interfaces';
+import { ElasticDbClientsConfig, FeatureCollection } from '../../common/interfaces';
 import { Item } from './item';
 
 @injectable()
@@ -19,7 +19,10 @@ export class ItemManager {
 
   public async getItems(itemQueryParams: ItemQueryParams, reduceFuzzyMatch = false, size?: number): Promise<FeatureCollection<Item>> {
     let elasticResponse: estypes.SearchResponse<Item> | undefined = undefined;
-    elasticResponse = await this.itemRepository.getItems(itemQueryParams, size ?? this.config.get<number>('db.elastic.searchy.properties.size'));
+    elasticResponse = await this.itemRepository.getItems(
+      itemQueryParams,
+      size ?? this.config.get<ElasticDbClientsConfig>(elasticConfigPath).control.properties.defaultResponseLimit
+    );
 
     const formattedResponse = formatResponse(elasticResponse);
 
