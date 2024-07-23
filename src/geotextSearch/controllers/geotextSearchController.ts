c26a0f9bdf13282678c682d91f5713be1754865d
Niv Greenstein <niv@rafael.co.il>
2024-07-23 20:33:30 +0300

    feat: created geotext controller

---
+++ src/geotextSearch/controllers/geotextSearchController.ts
@@ -0,0 +1,44 @@
+import { Logger } from "@map-colonies/js-logger";
+import { BoundCounter, Meter } from "@opentelemetry/api-metrics";
+import { RequestHandler } from "express";
+import httpStatus from "http-status-codes";
+import { injectable, inject } from "tsyringe";
+import { SERVICES } from "../../common/constants";
+import { GeotextSearchManager } from "../models/queryManager";
+import { GetQueryQueryParams, QueryResult } from "../interfaces";
+
+type GetGeotextSearchHandler = RequestHandler<
+  unknown,
+  QueryResult | { message: string; error: string }, //response
+  undefined,
+  GetQueryQueryParams
+>;
+
+type GetRegionshHandler = RequestHandler<unknown, string[], undefined, undefined>;
+
+@injectable()
+export class geotextSearchController {
+  private readonly createdResourceCounter: BoundCounter;
+
+  public constructor(
+    @inject(SERVICES.LOGGER) private readonly logger: Logger,
+    @inject(GeotextSearchManager) private readonly manager: GeotextSearchManager,
+    @inject(SERVICES.METER) private readonly meter: Meter
+  ) {
+    this.createdResourceCounter = meter.createCounter("created_resource");
+  }
+
+  public getGeotextSearch: GetGeotextSearchHandler = async (req, res, next) => {
+    try {
+      const response = await this.manager.search(req.query);
+      return res.status(httpStatus.OK).json(response);
+    } catch (error: unknown) {
+      this.logger.error("Error in getGeotextSearch", error);
+      next(error);
+    }
+  };
+
+  public getRegions: GetRegionshHandler = (req, res, next) => {
+    return res.status(httpStatus.OK).json(this.manager.regions());
+  };
+}
