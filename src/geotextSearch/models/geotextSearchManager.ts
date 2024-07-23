a92abfd2084d037a04ef057edf882c8b64547073
Niv Greenstein <niv@rafael.co.il>
2024-07-23 20:42:32 +0300

    feat: created geotextSearch manager

---
+++ src/geotextSearch/models/geotextSearchManager.ts
@@ -0,0 +1,68 @@
+import { IConfig } from 'config';
+import { Logger } from '@map-colonies/js-logger';
+import { inject, injectable } from 'tsyringe';
+import { SERVICES, elasticConfigPath } from '../../common/constants';
+import { GEOTEXT_REPOSITORY_SYMBOL, GeotextRepository } from '../DAL/geotextSearchRepository';
+import { GetQueryQueryParams, QueryResult, TextSearchParams } from '../interfaces';
+import { convertResult, parseGeo } from '../utils';
+import { IApplication } from '../../common/interfaces';
+import { ElasticDbClientsConfig } from '../../common/interfaces';
+
+@injectable()
+export class GeotextSearchManager {
+  public constructor(
+    @inject(SERVICES.LOGGER) private readonly logger: Logger,
+    @inject(SERVICES.APPLICATION) private readonly appConfig: IApplication,
+    @inject(SERVICES.CONFIG) private readonly config: IConfig,
+    @inject(GEOTEXT_REPOSITORY_SYMBOL) private readonly geotextRepository: GeotextRepository
+  ) {}
+
+  public async search(params: GetQueryQueryParams): Promise<QueryResult> {
+    const extractNameEndpoint = this.appConfig.services.tokenTypesUrl;
+    const {
+      geotext: geotextIndex,
+      placetypes: placetypesIndex,
+      hierarchies: hierarchiesIndex,
+    } = this.config.get<ElasticDbClientsConfig>(elasticConfigPath).geotext.properties.index as {
+      [key: string]: string;
+    };
+    const hierarchyBoost = this.appConfig.elasticQueryBoosts.hierarchy;
+
+    const [query, ...hierarchyQuery] = params.query.split(',');
+
+    const promises = Promise.all([
+      this.geotextRepository.extractName(extractNameEndpoint, query),
+      this.geotextRepository.generatePlacetype(placetypesIndex, query),
+      this.geotextRepository.extractHierarchy(hierarchiesIndex, hierarchyQuery.join(','), hierarchyBoost),
+    ]);
+
+    const [name, { placeTypes, subPlaceTypes }, hierarchies] = await promises;
+
+    const searchParams: TextSearchParams = {
+      query,
+      limit: params.limit,
+      sources: params.source ? (params.source instanceof Array ? params.source : [params.source]) : undefined,
+      viewbox: params.viewbox ? parseGeo(params.viewbox) : undefined,
+      boundary: params.boundary ? parseGeo(params.boundary) : undefined,
+      name,
+      placeTypes,
+      subPlaceTypes,
+      hierarchies,
+      regions: params.region,
+    };
+
+    const esResult = await this.geotextRepository.geotextSearch(
+      geotextIndex,
+      searchParams,
+      this.config.get<ElasticDbClientsConfig>(elasticConfigPath).geotext.properties.textTermLanguage,
+      this.appConfig.elasticQueryBoosts
+    );
+
+    console.log(this.appConfig.regions);
+    return convertResult(searchParams, esResult.hits.hits, { sources: this.appConfig.sources, regionCollection: this.appConfig.regions });
+  }
+
+  public regions(): string[] {
+    return Object.keys(this.appConfig.regions ?? {});
+  }
+}
