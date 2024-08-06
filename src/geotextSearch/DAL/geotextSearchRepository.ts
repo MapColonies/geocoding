bd268c8efb1c518105c108d686a2b705f1088bd4
Niv Greenstein <niv@rafael.co.il>
2024-08-06 11:45:55 +0300

    fix(geotextSearchRepository): changed queryElastic to common function and injecting logger

--- src/geotextSearch/DAL/geotextSearchRepository.ts
+++ src/geotextSearch/DAL/geotextSearchRepository.ts
@@ -1,25 +1,30 @@
-import { Client, estypes } from '@elastic/elasticsearch';
+import { Logger } from '@map-colonies/js-logger';
+import { estypes } from '@elastic/elasticsearch';
 import { FactoryFunction } from 'tsyringe';
-import { elasticClientSymbol } from '../../common/elastic';
+import { ElasticClient } from '../../common/elastic';
 import { cleanQuery, fetchNLPService } from '../utils';
 import { TextSearchParams, TokenResponse } from '../interfaces';
 import { PlaceTypeSearchHit, HierarchySearchHit, TextSearchHit } from '../models/elasticsearchHits';
 import { BadRequestError } from '../../common/errors';
-import { ElasticClients, IApplication } from '../../common/interfaces';
+import { IApplication } from '../../common/interfaces';
+import { ElasticClients } from '../../common/elastic';
+import { queryElastic } from '../../common/elastic/utils';
+import { SERVICES } from '../../common/constants';
 import { hierarchyQuery, placetypeQuery, geotextQuery } from './queries';
 
 /* eslint-enable @typescript-eslint/naming-convention */
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const createGeotextRepository = (client: Client) => {
+const createGeotextRepository = (client: ElasticClient, logger: Logger) => {
   return {
     async extractName(endpoint: string, query: string): Promise<string> {
       const tokensRaw = cleanQuery(query);
-      const response = await fetchNLPService<TokenResponse>(endpoint, { tokens: tokensRaw });
+      const response = await fetchNLPService<Partial<TokenResponse>>(endpoint, { tokens: tokensRaw });
 
       const { tokens, prediction } = response[0];
 
       if (!tokens || !prediction) {
+        logger.error('No tokens or prediction');
         throw new BadRequestError('No tokens or prediction');
       }
 
@@ -31,7 +36,7 @@ const createGeotextRepository = (client: Client) => {
     async generatePlacetype(index: string, query: string): Promise<{ placeTypes: string[]; subPlaceTypes: string[] }> {
       const {
         hits: { hits },
-      } = await queryElastic<PlaceTypeSearchHit>(client, index, placetypeQuery(query));
+      } = await queryElastic<PlaceTypeSearchHit>(client, { index, ...placetypeQuery(query) });
 
       if (hits.length == 2 && hits[0]._score! - hits[1]._score! > 0.5) {
         hits.pop();
@@ -48,7 +53,7 @@ const createGeotextRepository = (client: Client) => {
     async extractHierarchy(index: string, query: string, hierarchyBoost: number): Promise<HierarchySearchHit[]> {
       const {
         hits: { hits },
-      } = await queryElastic<HierarchySearchHit>(client, index, hierarchyQuery(query));
+      } = await queryElastic<HierarchySearchHit>(client, { index, ...hierarchyQuery(query) });
 
       const filteredHits = hits.length > 3 ? hits.filter((hit) => hit._score! >= hits[2]._score!) : hits;
 
@@ -67,25 +72,19 @@ const createGeotextRepository = (client: Client) => {
       textLanguage: string,
       elasticQueryBoosts: IApplication['elasticQueryBoosts']
     ): Promise<estypes.SearchResponse<TextSearchHit>> {
-      const response = await queryElastic<TextSearchHit>(client, index, geotextQuery(params, textLanguage, elasticQueryBoosts));
+      const response = await queryElastic<TextSearchHit>(client, { index, ...geotextQuery(params, textLanguage, elasticQueryBoosts) });
       return response;
     },
   };
 };
 
-const queryElastic = async <T>(client: Client, index: string, body: estypes.SearchRequest): Promise<estypes.SearchResponse<T>> => {
-  const response = await client.search<T>({
-    index,
-    ...body,
-  });
-
-  return response;
-};
-
 export type GeotextRepository = ReturnType<typeof createGeotextRepository>;
 
 export const geotextRepositoryFactory: FactoryFunction<GeotextRepository> = (depContainer) => {
-  return createGeotextRepository(depContainer.resolve<ElasticClients>(elasticClientSymbol).geotext);
+  return createGeotextRepository(
+    depContainer.resolve<ElasticClients>(SERVICES.ELASTIC_CLIENTS).geotext,
+    depContainer.resolve<Logger>(SERVICES.LOGGER)
+  );
 };
 
 export const GEOTEXT_REPOSITORY_SYMBOL = Symbol('GEOTEXT_REPOSITORY_SYMBOL');
