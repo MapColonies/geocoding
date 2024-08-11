2528bd86a7d34c44bc53d04f9561cff2c5b6b272
Niv Greenstein <greenstein9@gmail.com>
2024-08-11 20:04:33 +0300

    feat(routeController): updated controller based on new openapi

--- src/control/route/controllers/routeController.ts
+++ src/control/route/controllers/routeController.ts
@@ -7,16 +7,13 @@ import { injectable, inject } from 'tsyringe';
 import { SERVICES } from '../../../common/constants';
 import { RouteManager } from '../models/routeManager';
 import { Route } from '../models/route';
-import { FeatureCollection, GeoContext } from '../../../common/interfaces';
+import { CommonRequestParameters, FeatureCollection, GeoContext } from '../../../common/interfaces';
 
 type GetRoutesHandler = RequestHandler<undefined, FeatureCollection<Route>, undefined, GetRoutesQueryParams>;
 
-export interface GetRoutesQueryParams {
+export interface GetRoutesQueryParams extends CommonRequestParameters {
   command_name: string;
   control_point?: string;
-  geo_context?: string;
-  reduce_fuzzy_match?: string;
-  size?: string;
 }
 
 @injectable()
@@ -33,16 +30,14 @@ export class RouteController {
 
   public getRoutes: GetRoutesHandler = async (req, res, next) => {
     try {
-      const { command_name: commandName, control_point, geo_context, reduce_fuzzy_match, size } = req.query;
-      const response = await this.manager.getRoutes(
-        {
-          commandName,
-          controlPoint: control_point ? parseInt(control_point) : undefined,
-          geo: geo_context ? (JSON.parse(geo_context) as GeoContext) : undefined,
-        },
-        reduce_fuzzy_match == 'true',
-        size ? parseInt(size) : undefined
-      );
+      const { command_name: commandName, control_point, geo_context, disable_fuzziness, limit } = req.query;
+      const response = await this.manager.getRoutes({
+        commandName,
+        controlPoint: control_point ? parseInt(control_point) : undefined,
+        geo_context,
+        disable_fuzziness,
+        limit,
+      });
       return res.status(httpStatus.OK).json(response);
     } catch (error: unknown) {
       this.logger.warn('routeController.getRoutes Error:', error);
