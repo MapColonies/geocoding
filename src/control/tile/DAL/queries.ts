9e18e9d3cb7909dd0886d76dfbd43f54f2388d79
Niv Greenstein <greenstein9@gmail.com>
2024-08-11 16:29:37 +0300

    fix: fixed query to support new params

--- src/control/tile/DAL/queries.ts
+++ src/control/tile/DAL/queries.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { estypes } from '@elastic/elasticsearch';
-import { CommonRequestParameters } from '../../../common/interfaces';
+import { CommonRequestParameters, GeoContextMode } from '../../../common/interfaces';
+import { parseGeo } from '../../../geotextSearch/utils';
+import { BadRequestError } from '../../../common/errors';
 
 const ELASTIC_KEYWORDS = {
   TYPE: 'properties.TYPE.keyword',
   TILE_NAME: 'properties.TILE_NAME.keyword',
   SUB_TILE_ID: 'properties.SUB_TILE_ID.keyword',
+  GEOMETRY: 'geometry',
 };
 
 export interface TileQueryParams extends CommonRequestParameters {
@@ -13,28 +16,59 @@ export interface TileQueryParams extends CommonRequestParameters {
   subTile?: number;
 }
 
-export const queryForTiles = (params: Omit<TileQueryParams, 'subTile'>): estypes.SearchRequest => ({
-  query: {
-    bool: {
-      must: [
-        {
-          term: {
-            [ELASTIC_KEYWORDS.TYPE]: 'TILE',
-          },
-        },
-        {
-          match: {
-            [ELASTIC_KEYWORDS.TILE_NAME]: {
-              query: params.tile,
-              fuzziness: !params.disable_fuzziness ? 1 : undefined,
-              prefix_length: 1,
+export const queryForTiles = (params: Omit<TileQueryParams, 'subTile' | 'limit'>): estypes.SearchRequest => {
+  const { tile, geo_context, geo_context_mode, disable_fuzziness } = params;
+  if ((geo_context_mode !== undefined && geo_context === undefined) || (geo_context_mode === undefined && geo_context !== undefined)) {
+    throw new BadRequestError('/control/tiles/queryForTiles: geo_context and geo_context_mode must be both defined or both undefined');
+  }
+
+  const esQuery: estypes.SearchRequest = {
+    query: {
+      bool: {
+        must: [
+          {
+            term: {
+              [ELASTIC_KEYWORDS.TYPE]: 'TILE',
             },
           },
-        },
-      ],
+          {
+            match: {
+              [ELASTIC_KEYWORDS.TILE_NAME]: {
+                query: tile,
+                fuzziness: disable_fuzziness ? undefined : 1,
+                prefix_length: 1,
+              },
+            },
+          },
+        ],
+        // filter: [
+        //   {
+        //     geo_shape: {
+        //       geometry: {
+        //         shape: parseGeo(geo_context!),
+        //       },
+        //       boost: geo_context_mode === GeoContextMode.BIAS ? 1.1 : 1, //TODO: change magic number
+        //     },
+        //   },
+        // ],
+      },
     },
-  },
-});
+  };
+
+  if (geo_context !== undefined) {
+    esQuery.query!.bool![geo_context_mode === GeoContextMode.FILTER ? 'filter' : 'should'] = [
+      {
+        geo_shape: {
+          [ELASTIC_KEYWORDS.GEOMETRY]: {
+            shape: parseGeo(geo_context),
+          },
+          boost: geo_context_mode === GeoContextMode.BIAS ? 1.1 : 1, //TODO: change magic number
+        },
+      },
+    ];
+  }
+  return esQuery;
+};
 
 export const queryForSubTiles = (params: Required<TileQueryParams>): estypes.SearchRequest => ({
   query: {
