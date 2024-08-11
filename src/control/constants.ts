176fb1a8f2d79d34cfcacacf000fd46f5ad93a6b
Niv Greenstein <greenstein9@gmail.com>
2024-08-11 19:26:29 +0300

    feat(control/constants): moved CONTROL_FIELDS to this file and created ELASTIC_KEYWORDS as exported const

---
+++ src/control/constants.ts
@@ -0,0 +1,18 @@
+export const ELASTIC_KEYWORDS = {
+  type: 'properties.TYPE.keyword',
+  tileName: 'properties.TILE_NAME.keyword',
+  subTileId: 'properties.SUB_TILE_ID.keyword',
+  geometry: 'geometry',
+  objectCommandName: 'properties.OBJECT_COMMAND_NAME.keyword',
+};
+
+export const CONTROL_FIELDS = [
+  'type',
+  'geometry',
+  'properties.OBJECT_COMMAND_NAME',
+  'properties.TILE_NAME',
+  'properties.TYPE',
+  'properties.ENTITY_HEB',
+  'properties.SUB_TILE_ID',
+  'properties.SECTION',
+];
