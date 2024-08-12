622c77c675816ce1c09bf37de6e6ad6153e77a8c
Niv Greenstein <greenstein9@gmail.com>
2024-08-12 14:57:39 +0300

    feat(control/interfaces): created ControlResponse interface

---
+++ src/control/interfaces.ts
@@ -0,0 +1,17 @@
+import { Feature } from 'geojson';
+import { CommonRequestParameters, FeatureCollection } from '../common/interfaces';
+import { ConvertSnakeToCamelCase } from '../common/utils';
+
+export interface ControlResponse<T extends Feature> extends FeatureCollection<T> {
+  geocoding?: {
+    version?: string;
+    query?: ConvertSnakeToCamelCase<CommonRequestParameters>;
+    response: {
+      /* eslint-disable @typescript-eslint/naming-convention */
+      max_score: number;
+      results_count: number;
+      match_latency_ms: number;
+      /* eslint-enable @typescript-eslint/naming-convention */
+    };
+  };
+}
