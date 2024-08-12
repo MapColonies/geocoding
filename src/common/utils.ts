4432c03cd14ac9adf4f8920c8ec931258292ca46
Niv Greenstein <greenstein9@gmail.com>
2024-08-12 10:30:23 +0300

    feat: moved formatResponse function from common/utils to control/utils

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,34 +1,12 @@
-import { estypes } from '@elastic/elasticsearch';
 import proj4 from 'proj4';
-import { Item } from '../control/item/models/item';
-import { Tile } from '../control/tile/models/tile';
-import { Route } from '../control/route/models/route';
 import { utmProjection, wgs84Projection } from './projections';
-import { FeatureCollection, WGS84Coordinate } from './interfaces';
+import { WGS84Coordinate } from './interfaces';
 
 type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}` ? `${T}${Capitalize<SnakeToCamelCase<U>>}` : S;
 
 export type ConvertSnakeToCamelCase<T> = {
   [K in keyof T as SnakeToCamelCase<K & string>]: T[K];
 };
-
-export const formatResponse = <T extends Item | Tile | Route>(elasticResponse: estypes.SearchResponse<T>): FeatureCollection<T> => ({
-  type: 'FeatureCollection',
-  features: [
-    ...(elasticResponse.hits.hits.map((item) => {
-      const source = item._source;
-      if (source?.properties) {
-        Object.keys(source.properties).forEach((key) => {
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-          if (source.properties !== null && source.properties[key as keyof typeof source.properties] == null) {
-            delete source.properties[key as keyof typeof source.properties];
-          }
-        });
-      }
-      return source;
-    }) as T[]),
-  ],
-});
 
 export const validateWGS84Coordinate = (coordinate: { lon: number; lat: number }): boolean => {
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
--- src/control/utils.ts
+++ src/control/utils.ts
@@ -1,10 +1,37 @@
 import { estypes } from '@elastic/elasticsearch';
 import { parseGeo } from '../geotextSearch/utils';
-import { GeoContext, GeoContextMode, IConfig } from '../common/interfaces';
+import { FeatureCollection, GeoContext, GeoContextMode, IConfig } from '../common/interfaces';
 import { BadRequestError } from '../common/errors';
 import { elasticConfigPath } from '../common/constants';
 import { ElasticDbClientsConfig } from '../common/elastic/interfaces';
+import { Item } from '../control/item/models/item';
+import { Tile } from '../control/tile/models/tile';
+import { Route } from '../control/route/models/route';
 import { CONTROL_FIELDS, ELASTIC_KEYWORDS } from './constants';
+
+type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}` ? `${T}${Capitalize<SnakeToCamelCase<U>>}` : S;
+
+export type ConvertSnakeToCamelCase<T> = {
+  [K in keyof T as SnakeToCamelCase<K & string>]: T[K];
+};
+
+export const formatResponse = <T extends Item | Tile | Route>(elasticResponse: estypes.SearchResponse<T>): FeatureCollection<T> => ({
+  type: 'FeatureCollection',
+  features: [
+    ...(elasticResponse.hits.hits.map((item) => {
+      const source = item._source;
+      if (source?.properties) {
+        Object.keys(source.properties).forEach((key) => {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          if (source.properties !== null && source.properties[key as keyof typeof source.properties] == null) {
+            delete source.properties[key as keyof typeof source.properties];
+          }
+        });
+      }
+      return source;
+    }) as T[]),
+  ],
+});
 
 export const geoContextQuery = (
   geoContext?: GeoContext,
@@ -32,6 +59,7 @@ export const geoContextQuery = (
   };
 };
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const additionalControlSearchProperties = (config: IConfig, size: number): { size: number; index: string; _source: string[] } => ({
   size,
   index: config.get<ElasticDbClientsConfig>(elasticConfigPath).control.properties.index as string,
