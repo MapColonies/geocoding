ddc23760ff334211e34946d90a65d7d0fc197d63
Niv Greenstein <greenstein9@gmail.com>
2024-08-15 11:39:11 +0300

    fix(latlonRepository): added try-catch to getAll Request and thow ServiceUnavailableError

--- src/latLon/DAL/latLonRepository.ts
+++ src/latLon/DAL/latLonRepository.ts
@@ -3,14 +3,20 @@ import { Logger } from '@map-colonies/js-logger';
 import { DataSource } from 'typeorm';
 import { FactoryFunction } from 'tsyringe';
 import { SERVICES } from '../../common/constants';
+import { ServiceUnavailableError } from '../../common/errors';
 import { LatLon as LatLonDb } from './latLon';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const createLatLonRepository = (dataSource: DataSource, logger: Logger) => {
   return dataSource.getRepository(LatLonDb).extend({
     async getAll(): Promise<LatLonDb[]> {
-      const result = await this.find();
-      return result;
+      try {
+        const result = await this.find();
+        return result;
+      } catch (error: unknown) {
+        logger.error('Error in getAll function in latLonRepository', error);
+        throw new ServiceUnavailableError('Postgres client is not available. As for, the getAll request cannot be executed.');
+      }
     },
   });
 };
