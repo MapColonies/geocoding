c0861dcd755cea074cbd543e45c3778f0261b2b8
Niv Greenstein <greenstein9@gmail.com>
2024-08-20 15:40:48 +0300

    feat(control/interfaces): added G= any generic to be more typesafe at testing level. Not required when coding

--- src/control/interfaces.ts
+++ src/control/interfaces.ts
@@ -1,17 +1,15 @@
 import { Feature } from 'geojson';
 import { estypes } from '@elastic/elasticsearch';
 import { CommonRequestParameters, FeatureCollection } from '../common/interfaces';
-import { ConvertSnakeToCamelCase } from '../common/utils';
 
-export interface ControlResponse<T extends Feature> extends FeatureCollection<T> {
+export interface ControlResponse<T extends Feature, G = any> extends FeatureCollection<T> {
   geocoding?: {
     version?: string;
-    query?: ConvertSnakeToCamelCase<CommonRequestParameters>;
-    response: {
+    query?: G & CommonRequestParameters;
+    response: Pick<estypes.SearchHitsMetadata, 'max_score'> & {
       /* eslint-disable @typescript-eslint/naming-convention */
-      max_score: number;
-      results_count: number;
-      match_latency_ms: number;
+      results_count?: estypes.SearchHitsMetadata['total'];
+      match_latency_ms?: estypes.SearchResponse['took'];
       /* eslint-enable @typescript-eslint/naming-convention */
     };
   };
