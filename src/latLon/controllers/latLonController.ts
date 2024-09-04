92f19a8fd8ab1f070e5abf394e38dae080df8279
Niv Greenstein <greenstein9@gmail.com>
2024-09-04 18:37:19 +0300

    delete: removed unused code

--- src/latLon/controllers/latLonController.ts
+++ src/latLon/controllers/latLonController.ts
@@ -10,22 +10,9 @@ import { LatLonManager } from '../models/latLonManager';
 import { WGS84Coordinate } from '../../common/interfaces';
 /* istanbul ignore file */
 
-type GetLatLonToTileHandler = RequestHandler<undefined, { [key: string]: unknown } & Feature, undefined, GetLatLonToTileQueryParams>;
+type GetCoordinatesHandler = RequestHandler<undefined, { [key: string]: unknown } & Feature, undefined, GetCoordinatesRequestParams>;
 
-type GetLatLonToMgrsHandler = RequestHandler<undefined, { [key: string]: unknown } & Feature, undefined, GetLatLonToMgrsQueryParams>;
-
-type GetCoordinatesHandler = RequestHandler<
-  undefined,
-  { [key: string]: unknown } & Feature,
-  undefined,
-  WGS84Coordinate & { target_grid: 'control' | 'MGRS' }
->;
-
-export interface GetLatLonToTileQueryParams extends WGS84Coordinate {}
-
-export interface GetLatLonToMgrsQueryParams extends WGS84Coordinate {
-  accuracy?: number;
-}
+export type GetCoordinatesRequestParams = WGS84Coordinate & { target_grid: 'control' | 'MGRS' };
 
 @injectable()
 export class LatLonController {
@@ -38,31 +25,6 @@ export class LatLonController {
   ) {
     this.createdResourceCounter = meter.createCounter('created_resource');
   }
-
-  public latlonToTile: GetLatLonToTileHandler = async (req, res, next) => {
-    try {
-      const { lat, lon } = req.query;
-
-      const response = await this.manager.latLonToTile({ lat, lon });
-      return res.status(httpStatus.OK).json(response);
-    } catch (error: unknown) {
-      this.logger.warn('latLonController.latlonToTile Error:', error);
-      next(error);
-    }
-  };
-
-  public latlonToMgrs: GetLatLonToMgrsHandler = (req, res, next) => {
-    try {
-      const { lat, lon, accuracy } = req.query;
-
-      const response = this.manager.latLonToMGRS({ lat, lon, accuracy });
-
-      return res.status(httpStatus.OK).json(response);
-    } catch (error: unknown) {
-      this.logger.warn('latLonController.latlonToMgrs Error:', error);
-      next(error);
-    }
-  };
 
   public getCoordinates: GetCoordinatesHandler = async (req, res, next) => {
     try {
