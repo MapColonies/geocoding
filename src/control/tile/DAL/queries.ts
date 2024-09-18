28bf14551248e5511224434ee29ce32324e8a4db
Niv Greenstein <greenstein9@gmail.com>
2024-09-18 14:18:19 +0300

    fix: fixed should to be object or undefined.

--- src/control/tile/DAL/queries.ts
+++ src/control/tile/DAL/queries.ts
@@ -105,7 +105,7 @@ export const queryForTilesByBbox = ({
           },
           ...(geoCOntextQueryFilter ?? []),
         ],
-        should: should ?? {},
+        should,
       },
     },
   };
