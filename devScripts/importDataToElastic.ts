117ce76cadda6867d39ab917a9a9c0db87ed53ab
Niv Greenstein <niv@rafael.co.il>
2024-09-22 15:12:45 +0300

    fix: changed to node:crypto to specifically use the new nodejs crypto built in module

--- devScripts/importDataToElastic.ts
+++ devScripts/importDataToElastic.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 import { Client } from '@elastic/elasticsearch';
 import { ElasticDbClientsConfig } from '../src/common/elastic/interfaces';
 import { elasticConfigPath } from '../src/common/constants';
