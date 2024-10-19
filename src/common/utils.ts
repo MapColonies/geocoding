e27a1fcf181f133c57ff19222f1796e76525403e
Niv Greenstein <greenstein9@gmail.com>
2024-10-19 14:32:59 +0300

    delete: removed unused code

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -239,19 +239,6 @@ export const healthCheckFactory: FactoryFunction<void> = (container: DependencyC
     });
 };
 
-export const promiseTimeout = async <T>(ms: number, promise: Promise<T>): Promise<T> => {
-  // create a promise that rejects in <ms> milliseconds
-  const timeout = new Promise<T>((_, reject) => {
-    const id = setTimeout(() => {
-      clearTimeout(id);
-      reject(new TimeoutError(`Timed out in + ${ms} + ms.`));
-    }, ms);
-  });
-
-  // returns a race between our timeout and the passed in promise
-  return Promise.race([promise, timeout]);
-};
-
 export const parseGeo = (input: GeoContext | undefined): Geometry | undefined => {
   validateGeoContext(input);
   const geoContext = input as GeoContext;
