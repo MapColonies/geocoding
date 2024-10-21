24074251dddac37ce0c6bde932f85f1598d9738b
Niv Greenstein <greenstein9@gmail.com>
2024-10-21 12:32:26 +0300

    fix: added missing property

--- src/control/route/models/route.ts
+++ src/control/route/models/route.ts
@@ -5,6 +5,7 @@ export interface Route extends Feature {
   properties: GeoJsonProperties & {
     TYPE: 'ROUTE';
     OBJECT_COMMAND_NAME: string;
+    TIED_TO?: string;
     ENTITY_HEB: string;
     LAYER_NAME: string;
   };
