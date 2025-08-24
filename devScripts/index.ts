fe540869e37d55a2f62c1122d78a846f7c214aec
NatalieShaked <shakednatalie@gmail.com>
2025-08-24 12:57:21 +0300

    fix: devscript config changes

--- devScripts/index.ts
+++ devScripts/index.ts
@@ -1,13 +1,25 @@
-import { getConfig } from '../src/common/config';
+import { config } from '@map-colonies/config';
+import { vectorGeocodingV1 } from '@map-colonies/schemas';
 import importDataToElastic from './importDataToElastic';
 import importDataToS3 from './importDataToS3';
+import { ConfigType } from '../src/common/config';
 
-const config = getConfig();
+async function main() {
+  const configInstance: ConfigType = await config({
+    schema: vectorGeocodingV1,
+    offlineMode: true,
+  });
 
-importDataToElastic(config)
-  .then(() => console.log('Success import data to elastic'))
-  .catch(console.error);
+  importDataToElastic(configInstance)
+    .then(() => console.log('Success import data to elastic'))
+    .catch(console.error);
 
-importDataToS3(config)
-  .then(() => console.log('Success import data to s3'))
-  .catch(console.error);
+  importDataToS3(configInstance)
+    .then(() => console.log('Success import data to s3'))
+    .catch(console.error);
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
